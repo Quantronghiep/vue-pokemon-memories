0f386c4b8fc4dbb8c32754f72ae6f8017e9c27e3
Quan Trong Hiep <quantronghiep2001@gmail.com>
2024-05-13 22:28:45 +0700

    game pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <main-screen v-if="statusMatch === 'default'" @onStart="onHandleBeforeStart($event)"/>
+  <interact-screen v-if="statusMatch === 'match'" :cardsContext="settings.cardsContext" @onFinish="onGetResult()" />
+  <result-screen v-if="statusMatch === 'result'" :timer="timer" @onStartAgain="statusMatch = 'default'" />
+  <copy-right />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainScreen from './components/MainScreen.vue'
+import InteractScreen from '@/components/InteractScreen'
+import ResultScreen from '@/components/ResultScreen'
+import CopyRight from '@/components/CopyRight'
+import {shuffled} from "./utils/array"
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+    CopyRight
+  },
+  data(){
+    return {
+      settings: {
+        totalOfBlocks : 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "default",
+      timer: 0,
+    }
+  },
+  methods: {
+    onHandleBeforeStart(config){
+      console.log("running HandleBeforeStart ", config);
+      // data ready 
+      this.settings.totalOfBlocks = config.totalOfBlocks;
+      const fisrtCards =  Array.from({length: this.settings.totalOfBlocks / 2}, (_,i) => i+1);
+      const secondCards =  [...fisrtCards]; // clone shadow
+      const cards = [...fisrtCards, ...secondCards];
+      
+      this.settings.cardsContext = shuffled(shuffled(shuffled(shuffled(cards)))); // dao lon
+      this.settings.startedAt = new Date().getTime();
+
+      this.statusMatch = "match";
+    },
+
+    onGetResult(){
+      //get timer
+      this.timer = new Date().getTime() - this.settings.startedAt;
+      //switch to result component
+      this.statusMatch = 'result';
+    }
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CopyRight.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="copy-right">
+    <p>
+        This game owned by HIEPQT204 - <a href="#">view here</a>
+    </p>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.copy-right {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+
+.copy-right a {
+  color: #f4dc26;
+}
+</style>
